a6411a32b027a6c260da0dbd09737465a28aec84
Subhajit Kundu <subha60kundu@gmail.com>
2024-07-13 22:32:51 +0530

    feat: Adding support for customized timer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import {Button} from '@/components/ui/button'
 import {Dialog, DialogTrigger} from '@/components/ui/dialog'
-import PPTUploaderDialog from '@/app/PPTUploaderDialog'
+import PPTUploaderDialog from '@/components/dialogs/PPTUploaderDialog'
 
 export default function Home() {
 	return (
--- components/dialogs/PPTUploaderDialog.tsx
+++ components/dialogs/PPTUploaderDialog.tsx
@@ -5,9 +5,16 @@ import SlidesUploader from '@/components/providers/SlidesUploader'
 import {useWords} from '@/components/providers/GlobalStateProvider'
 import {Button} from '@/components/ui/button'
 import Link from 'next/link'
+import {Input} from '@/components/ui/input'
+import {useState} from 'react'
 
 const PPTUploaderDialog = () => {
 	const {words} = useWords()
+	const [timer, setTimer] = useState(15)
+
+	const handleTimerChange = (timer: number) => {
+		setTimer(timer)
+	}
 
 	return (
 		<DialogContent>
@@ -24,12 +31,16 @@ const PPTUploaderDialog = () => {
 
 			{words.length > 0 && (
 				<div className="mt-4">
-					<p className="text-sm text-muted-foreground text-center">
+					<p className="text-sm text-muted-foreground">
 						<span className="text-primary">{words.length}</span> words found in the presentation file.
 					</p>
-					<Link href="/cycle">
-						<Button size="sm" className="w-full mt-3">Start practice</Button>
-					</Link>
+
+					<div className="mt-3 flex items-stretch justify-between gap-2">
+						<Input className="w-24" placeholder="Timer" type="number" value={timer} onChange={event => handleTimerChange(Number(event.target.value))} />
+						<Link href={`/cycle?timer=${timer}`} className="w-full">
+							<Button size="sm" className="w-full border border-primary">Start practice</Button>
+						</Link>
+					</div>
 				</div>
 			)}
 		</DialogContent>
