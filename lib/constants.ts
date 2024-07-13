a968013370623832fa7ea7d006643e2d13d7daf7
Subhajit Kundu <subha60kundu@gmail.com>
2024-07-13 23:17:10 +0530

    chore: Changing repo address

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,2 @@
-export const defaultUrl = process.env.VERCEL_URL ? 'https://tickernow.xyz' : process.env.MAIN_URL || 'http://localhost:3000'
-export const githubRepo = 'https://github.com/pingSubhajit/ticker'
+export const githubRepo = 'https://github.com/pingSubhajit/watpad'
 export const portfolio = 'https://subhajitkundu.me'
