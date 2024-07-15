8cf1c60d75d3b5a1dfe39d5c24ff8ef4c8fe3303
Subhajit Kundu <subha60kundu@gmail.com>
2024-07-15 12:48:38 +0530

    fix: Changing emoji to sparkle in the hero pill

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -14,7 +14,7 @@ const Pill = ({ href, title }: { href: string; title: string }) => {
 						'p-px ![mask-composite:subtract]',
 					)}
 				/>
-				🎉 <Separator className="mx-2 h-4" orientation="vertical" />
+				✨ <Separator className="mx-2 h-4" orientation="vertical" />
 				<span
 					className={cn(
 						'animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent',
